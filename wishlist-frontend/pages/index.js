33659033a43eca426cd7aeac3f7fc33c79b6a707
Kayode <bkaoluborode@gmail.com>
2021-05-19 19:45:33 +0100

    chore: Install next.js framework

---
+++ wishlist-frontend/pages/index.js
@@ -0,0 +1,26 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>Create Next App</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous"></link>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <nav class="navbar navbar-light bg-light">
+        <div class="container-fluid">
+          <span class="navbar-brand mb-0 h1">Wish List</span>
+        </div>
+      </nav>
+
+      <main>
+        
+      </main>
+
+      <footer>
+
+      </footer>
+    </div>
+  )
+}
