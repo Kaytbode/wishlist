1066cbbb0e539b6198dbee190fbc3a230001f81e
Kayode <bkaoluborode@gmail.com>
2021-05-20 15:48:01 +0100

    chore: Set up Azure functions as backend

--- wishlist-frontend/pages/index.js
+++ wishlist-frontend/pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 
 export default function Home() {
   return (
-    <div className="container">
+    <div>
       <Head>
         <title>Create Next App</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous"></link>
