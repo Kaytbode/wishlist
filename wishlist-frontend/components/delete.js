32374e4714b939c64c115fa26ba22c72a270923b
Kayode <bkaoluborode@gmail.com>
2021-06-02 10:05:57 +0100

    feat: Confirmation of delete request

--- wishlist-frontend/components/delete.js
+++ wishlist-frontend/components/delete.js
@@ -1,8 +1,15 @@
 import { useRouter } from 'next/router'
+import { useState } from 'react'
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
 
-export default function Delete({ wish }) {
+export default function Delete({ wish }) { 
     const router = useRouter();
 
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
 
     const deleteWish = async event => {
       event.preventDefault()
@@ -18,15 +25,32 @@ export default function Delete({ wish }) {
       )
 
       const result = await res.json()
-      console.log(result)
+
+      handleClose()
 
       router.push('/allwishes')
     }
 
     return (
-        <form onSubmit={deleteWish}>
-            <button type="submit" class="btn btn-danger">Delete Wish</button>
-        </form>
+        <div>
+          <Button variant="danger" onClick={handleShow}>
+            Delete Wish
+          </Button>
+
+          <Modal show={show} onHide={handleClose}>
+            <Modal.Header closeButton>
+            <Modal.Title>Delete Wish</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>Are you sure you want to delete this wish?</Modal.Body>
+            <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+                Close
+            </Button>
+            <Button variant="danger" onClick={deleteWish}>
+                Confirm Delete
+            </Button>
+            </Modal.Footer>
+          </Modal>
+        </div>
     )
 }
-
