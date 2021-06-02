a385b8d6fe8f64e5759910b28a8113d16a795fdd
Kayode <bkaoluborode@gmail.com>
2021-06-02 10:19:04 +0100

    chore: Remove redundant script links

--- wishlist-frontend/pages/allwishes.js
+++ wishlist-frontend/pages/allwishes.js
@@ -9,12 +9,6 @@ export default function AllWishes({ wishes }) {
             <Head>
                 <title>A Wish List App</title>
                 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous"></link>
-                <link
-                    rel="stylesheet"
-                    href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
-                    integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l"
-                    crossorigin="anonymous"
-                />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
             <nav class="navbar navbar-light bg-light">
@@ -54,8 +48,6 @@ export default function AllWishes({ wishes }) {
                     ))}
                 </div>
             </main>
-            <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js" integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossorigin="anonymous"></script>
         </div>
     )
 }
